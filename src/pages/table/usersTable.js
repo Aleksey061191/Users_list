3903e27d84aa44e361de4a91e532a744bf3d197c
LEXA\Aleksey <lexa.11@list.ru>
2021-08-18 13:19:13 +0300

    added username

--- src/pages/table/usersTable.js
+++ src/pages/table/usersTable.js
@@ -6,13 +6,13 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import StarIcon from '@material-ui/icons/Star';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
       margin: 'auto',
       width: '100%',
-      maxWidth: 360,
+      maxWidth: 400,
       backgroundColor: theme.palette.background.paper,
     },
   }));
@@ -36,15 +36,17 @@ const TablePage = () => {
             
             <List component="nav" className={classes.root} aria-label="contacts">
                 
-              {users.map(item=>{
-                
+              {users.map(item=>{ 
                 return(
                   <ListItem >
                     <ListItemIcon>
-                      
+                      {`${item.id}.`}
                     </ListItemIcon>
-                    <ListItemText  primary={item.name} />
+                    <ListItemText style={{width:''}}  primary={item.name} />
+                    <ListItemText  style={{color:"rgba(0, 0, 0, 0.54)"}} primary={`@${item.username}`} />
+                    
                   </ListItem>
+                  
                 ) 
               })}
               
